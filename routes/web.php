c51e026e066be83dac8c1cb5c6d4a01bc97af81a
Stalin Sánchez <ssanchez684@gmail.com>
2021-02-21 04:10:19 +0000

    Basic Auth

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::post('statuses', [StatusController::class, 'store'])->name('statuses.store');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
