d0c63cc7e2833ed78854c5a1ac50f98f592c7deb
Stalin Sánchez <ssanchez684@gmail.com>
2021-03-09 02:54:02 +0000

    66 - Comment created event test

---
+++ app/Events/CommentCreatedEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class CommentCreatedEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * CommentResource from comment created
+     *
+     * @var \App\Http\Resources\CommentResource
+     */
+    public $comment;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \App\Http\Resources\CommentResource $comment
+     * @return void
+     */
+    public function __construct($comment)
+    {
+        $this->comment = $comment;
+
+        $this->dontBroadcastToCurrentUser();
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
